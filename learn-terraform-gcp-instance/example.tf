314f687febd71b16ab09f7a4264ed2691a732b56
Tamara Bartlett <tamara.bartlett@uplight.com>
2020-09-18 10:49:33 -0600

    Day 2:

--- learn-terraform-gcp-instance/example.tf
+++ learn-terraform-gcp-instance/example.tf
@@ -1,22 +1,34 @@
 terraform {
   required_providers {
-    gcp = {
-        source = "hashicorp/google"
-        version = "~> 3.36.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.36.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("account.json")
+  credentials = file("terraform-practice-287722-c3b668866e56.json")
   project     = "terraform-practice-287722"
   region      = "us-west3"
+  zone        = "us-west3-b"
 }
 
-resource "google_compute_instance" "vm_instance"{
-  name = "test_instance"
+resource "google_compute_network" "vpc_network"{
+  name = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "tamara-test"
   machine_type = "f1-micro"
-  zone = "us-west3-a"
+  zone         = "us-west3-b"
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+
+    }
+  }
 
   boot_disk {
     initialize_params {
